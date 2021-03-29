455d44bfba8d8d6c3a8ab3a7372222f97f89c126
Rokiatou <rssissoko600@gmail.com>
2021-03-29 04:24:28 +0000

    Here is the basic framework for 'Peer Advice' page

---
+++ src/main/java/com/google/servlets/NewAdviceServlet.java
@@ -0,0 +1,23 @@
+package com.google.sps.servlets;
+
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.FullEntity;
+import com.google.cloud.datastore.KeyFactory;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
+
+/** Servlet responsible for creating new advice. */
+@WebServlet("/new-advice")
+public class NewTaskServlet extends HttpServlet {
+    Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+}
+
+// Modify your servlet file to take the data entered by users in your "contact me" 
+// form and store it as an entity in Datastore. Get this working before you worry about loading the data.
